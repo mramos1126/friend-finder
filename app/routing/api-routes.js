d03d56dbedcf920331d6ee4216d535660ccdb8ff
Mauricio <Ramos@Mauricios-MBP.home>
2016-05-03 20:33:31 -0400

    homework3 again

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -20,14 +20,25 @@ app.post('/api/friends', function(req, res){
 
 
 	res.json(friend);
-//break into variables
-//this will show the data that was posted from the survey
-for (i= 0 ; i<friendData.length; i++){if (friendData[i]== friendData[i]){
-if (res.score < 100){ friendData[i]== friendData[i]}
 
+//this will show the data that was posted from the survey. This will be a for-loop that this display the content that the client posted on the app. One for loop will bring up the the data. The second for-loop will break down the score and subtract a json from the api. IntThe last for-loop, I attempt to   gather the difference and match with  one of the jsons from the api. 
+
+
+for (i= 0 ; i<friendData.length; i++){
+
+newFriend= {};
+totalDifference =100;
+for (j = 0; j<friendData.scores[h]; j++){
+(newFriend).push(req.body.scores)-(friendData[i].scores[h])
+
+}
+for (k = 0; k<totalDifference.length; k++){
+if (newFriend == totalDifference ){
+	newFriend = friendData
 }
 
 
+}
 
 	 }
 
